f1f0781635f4ef9a8bb2d04bf4a4ebdaddf45db6
Sajan Sunny <sajan.sunny@outlook.com>
2021-02-12 07:09:52 +0530

    Updated

--- pythonSelenium/demoBrowser.py
+++ pythonSelenium/demoBrowser.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome(executable_path="C:\\Drivers\\chromedriver.exe")
+#driver = webdriver.Firefox(executable_path="C:\\Drivers\\geckodriver.exe")
+#driver = webdriver.Ie(executable_path="C:\\Drivers\\IEDriverServer.exe")
+driver.maximize_window()
 driver.get("https://rahulshettyacademy.com/")
 print(driver.title)
 print(driver.current_url)
+driver.get("https://www.udemy.com/")
+driver.minimize_window()
+driver.back()
+driver.refresh()
 driver.close()
